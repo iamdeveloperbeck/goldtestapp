5f1d234e6262461fa0a054df742de14102632e1f
Developer Beck <85181067+iamdeveloperbeck@users.noreply.github.com>
2024-11-07 10:51:50 +0500

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -32,28 +32,28 @@ function Login() {
   };
 
   return (
-    <div>
+    <div className="w-full h-screen flex items-center justify-center flex-col">
       <h1 className='text-4xl font-bold mb-4'>Admin Kirish</h1>
       <form onSubmit={handleLogin}>
         <div className='mb-4'>
-          <label className='block text-sm font-medium text-gray-700'>Email</label>
+          <label className='block text-sm font-medium text-gray-900'>Email</label>
           <input
             type='email'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className='border p-2 w-full'
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
           />
         </div>
         <div className='mb-4'>
-          <label className='block text-sm font-medium text-gray-700'>Parol</label>
+          <label className='block text-sm font-medium text-gray-900'>Parol</label>
           <input
             type='password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className='border p-2 w-full'
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
           />
         </div>
-        <button type='submit' className='bg-blue-500 text-white p-2 rounded'>Kirish</button>
+        <button type='submit' className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'>Kirish</button>
       </form>
       <button onClick={handleGoogleLogin} className='bg-red-500 text-white p-2 rounded mt-4'>
         <FaGoogle color='#fff' /> Google orqali kirish
