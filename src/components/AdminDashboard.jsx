aedcdf4ff8855716a39826e8d1cce4e90fce2e3c
Developer Beck <85181067+iamdeveloperbeck@users.noreply.github.com>
2025-02-28 10:29:01 +0500

    Update AdminDashboard.jsx

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -103,7 +103,7 @@ function AdminDashboard() {
     const doc = new jsPDF();
     const date = new Date()
     const findCategory = categories?.find(item => item.id === filterCategory)
-    doc.text(`${findCategory?.name}       ${formatDate(date)}`, 20, 10);
+    doc.text(`${findCategory?.name} ${formatDate(date)}`, 20, 10);
     doc.autoTable({
       head: [['Ism va familiya', 'To\'g\'ri javoblar', 'Noto\'g\'ri javoblar', 'Holat']],
       body: filteredUsers.map(user => [
